75b4b70f4366e1b4a4c90495d24af6a35a5c6051
Prakash Joshi <prakash@codepiemedia.com>
2021-12-25 22:59:16 +0530

    - space

--- app/src/main/java/com/pra/birddemo/MainActivity.java
+++ app/src/main/java/com/pra/birddemo/MainActivity.java
@@ -24,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
         setUpModel();
         setUpPlane();
